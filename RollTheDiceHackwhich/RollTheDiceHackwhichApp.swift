3c97af631aa9f30fa8c65a789cf64f44b0cf36c0
William Dunkel <wndunkel@gmail.com>
2023-10-25 09:46:26 -0500

    Initial Commit

---
+++ RollTheDiceHackwhich/RollTheDiceHackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceHackwhichApp.swift
+//  RollTheDiceHackwhich
+//
+//  Created by William Dunkel on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceHackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
