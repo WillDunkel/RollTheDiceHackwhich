2e66351d621a6573e5ef890e23101933d2ca3202
William Dunkel <wndunkel@gmail.com>
2023-10-26 09:53:19 -0500

    Added dice images

--- RollTheDiceHackwhich/ContentView.swift
+++ RollTheDiceHackwhich/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Dice Roll")
+                .font(.title)
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                   
+            }
+            Spacer()
         }
-        .padding()
     }
 }
 
